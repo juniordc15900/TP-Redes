9571a830483d745ba39b60a31b81ab561e841d44
juniordc15900 <juniordc15900@gmail.com>
2023-06-16 16:52:52 -0300

    algumas alterações

--- ssocket.py
+++ ssocket.py
@@ -4,9 +4,9 @@ def cria_socket(host,porta):
     s_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     
-    # Associa o soquete do servidor ao endereço e porta fornecidos
+    # Associa o socket do servidor ao endereço e porta fornecidos
     s_socket.bind((host, porta))
     
-    # Inicia a escuta do soquete do servidor
+    # Inicia a escuta do socket do servidor
     s_socket.listen(1)
     return s_socket
